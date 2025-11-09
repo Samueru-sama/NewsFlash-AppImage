29edfe71c0ec0279234906065852424dc9a3531c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-09 08:19:26 -0400

    mmm

--- newsflash-appimage.sh
+++ newsflash-appimage.sh
@@ -31,9 +31,6 @@ wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 ./quick-sharun /usr/bin/newsflash /usr/bin/xdg-dbus-proxy /usr/bin/bwrap
 
-# wtf
-cp -r ./AppDir/shared ./AppDir/shared/lib/webkitgtk-6.0
-
 ## Set gsettings to save to keyfile, instead to dconf
 echo "GSETTINGS_BACKEND=keyfile" >> ./AppDir/.env
 
